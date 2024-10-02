20a38a5dd2cbcff762f50eb0f7a684276c845991
Kako <espinosamillapro@gmail.com>
2024-10-01 23:21:03 -0300

    Primer push

---
+++ app/src/main/java/com/example/actividad2_2/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.actividad2_2;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        Button downloadButton = findViewById(R.id.downloadButton);
+
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        final Bitmap bitmap = loadImageFromNetwork("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSDiNatrWCuOlQr5ehqf7q3xJc_BWJm4dR6g&s");
+                        imageView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                imageView.setImageBitmap(bitmap);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+    }
+
+
+    private Bitmap loadImageFromNetwork(String url) {
+        Bitmap bitmap = null;
+        try {
+            java.net.URL imageUrl = new java.net.URL(url);
+            HttpURLConnection connection = (HttpURLConnection) imageUrl.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            bitmap = BitmapFactory.decodeStream(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+}
